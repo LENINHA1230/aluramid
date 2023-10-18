ee02bbb0aed33b0faffb5b8fa76228ec5e5cd42d
LENINHA1230 <130582458+LENINHA1230@users.noreply.github.com>
2023-10-18 08:15:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocasompom(){
+    document.queryselector('#som_tecla_pom').play('');
+}
+document.querySelector('.tecla_pom'); 
+
+
+document.querySelector('#som_tecla_pom').play('');
+document.querySelector('.tecla_pom').onclick= tocasompom ();
