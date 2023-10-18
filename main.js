6815cf619f2852de62ddeb7c3fe7373ea4539916
LENINHA1230 <130582458+LENINHA1230@users.noreply.github.com>
2023-10-18 12:35:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ function tocasompom(){
     document.queryselector('#som_tecla_pom').play('');
 }
 document.querySelector('.tecla_pom'); 
+ const listadeteclas= document.querySelectorAll('.tecla');
 
 
-document.querySelector('#som_tecla_pom').play('');
-document.querySelector('.tecla_pom').onclick= tocasompom ();
+    listadeteclas[0].onclick= tocasompom;
+
+
+for(contador = 0; contador< listadeteclas.length;contador ++){
+    const instrumento = listadeteclas[contador].classList[1];
+}
+ const tecla = listadeteclas[contador];
+
+   const idaudio = #som_${instrumento};
